0c46121d5368a33abcf9c10326a1976d081335c6
fengfeng8867dd@hotmail.com <fengfeng8867dd@hotmail.com>
2019-11-01 17:36:42 -0600

    refactor

--- Scraper/Form1.cs
+++ Scraper/Form1.cs
@@ -82,37 +82,58 @@ namespace Scraper
                     {
                         var parent = child.FindElement(By.XPath("./parent::*")); // <tr> that is sec 001
                         var cols = parent.FindElements(By.TagName("td")); //re-dive into the <tr> to filter course numbers
+                        var cat = cols[2].Text; // category
 
-                        // Filter out the correct course numbers, <td>s in a <tr> that fits the course number constraint
-                        foreach (var d in cols)
+                        // Filter out the correct course numbers
+                        int number = TryToParse(cat);
+                        if (!(number <= TryToParse(ConfigurationManager.AppSettings["CourseRangeEnd"]) &&
+                            number >= TryToParse(ConfigurationManager.AppSettings["CourseRangeStart"])))
                         {
-                            int number = TryToParse(d.Text);
-                            if (number <= TryToParse(ConfigurationManager.AppSettings["CourseRangeEnd"]) &&
-                                number >= TryToParse(ConfigurationManager.AppSettings["CourseRangeStart"]))
-                            {
-                                //Filter out the components , <tr> that fits the filter
-                                var filter = ConfigurationManager.AppSettings["Filters"].Split(',');
-                                // richTextBox1.AppendText(number.ToString());
-                                var tmpParent = d.FindElement(By.XPath("./parent::*"));
-                                var l = tmpParent.FindElements(By.TagName("td"));
-                                //foreach (var c in l)
-                                //{
-                                  richTextBox1.AppendText(l[2].Text + ", " + l[3].Text +  ", " + l[4].Text + ", " + l[5].Text);
-                                //}
-                                richTextBox1.AppendText("\n");
-
-                                // Ones that does not need
-                                //var f = tmpParent.FindElements(By.XPath("//td[contains(text(),'" + filter[0] + "')]"));
-                                  // + " or contains(text(),'" + filter[1] + "')]"));
-
-                                // if f did not find any thing, then it's the final filtered version
-                                //if (f.Count == 0)
-                                //{
-                                //    filteredRows.Add(tmpParent);
-                                //}
-
-                            }
+                            continue;
                         }
+
+                        // Filter out key words
+                        var filter = ConfigurationManager.AppSettings["Filters"].Split(',');
+                        var title = cols[4].Text;
+                        if(title.Contains(filter[0]) || title.Contains(filter[1]))
+                        {
+                            continue;
+                        }
+
+                        // At this point it's a valid row
+                        richTextBox1.AppendText(cat + ", " + title + "\n");
+
+                           // Console.WriteLine();
+                           // Filter out the correct course numbers, <td>s in a <tr> that fits the course number constraint
+                           //foreach (var d in cols)
+                           //{
+                           //    int number = TryToParse(d.Text);
+                           //    if (number <= TryToParse(ConfigurationManager.AppSettings["CourseRangeEnd"]) &&
+                           //        number >= TryToParse(ConfigurationManager.AppSettings["CourseRangeStart"]))
+                           //    {
+                           //        //Filter out the components , <tr> that fits the filter
+                           //        var filter = ConfigurationManager.AppSettings["Filters"].Split(',');
+                           //        // richTextBox1.AppendText(number.ToString());
+                           //        var tmpParent = d.FindElement(By.XPath("./parent::*"));
+                           //        var l = tmpParent.FindElements(By.TagName("td"));
+                           //        //foreach (var c in l)
+                           //        //{
+                           //          richTextBox1.AppendText(l[2].Text + ", " + l[3].Text +  ", " + l[4].Text + ", " + l[5].Text);
+                           //        //}
+                           //        richTextBox1.AppendText("\n");
+
+                            //        // Ones that does not need
+                            //        //var f = tmpParent.FindElements(By.XPath("//td[contains(text(),'" + filter[0] + "')]"));
+                            //          // + " or contains(text(),'" + filter[1] + "')]"));
+
+                            //        // if f did not find any thing, then it's the final filtered version
+                            //        //if (f.Count == 0)
+                            //        //{
+                            //        //    filteredRows.Add(tmpParent);
+                            //        //}
+
+                            //    }
+                            //}
                     }
 
                     //foreach (var r in filteredRows)
@@ -156,6 +177,10 @@ namespace Scraper
 
         }
     }
+}
+public class Data
+{
+
 }
 
 // reference: https://stackoverflow.com/questions/6992993/selenium-c-sharp-webdriver-wait-until-element-is-present
